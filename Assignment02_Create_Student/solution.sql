f3d04bc7dae359f6bec3fc7a605dfe73efd6c78a
1U25CA083-SANTHOSH <santhosh732008@gmail.com>
2026-07-25 13:31:19 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -2,16 +2,12 @@ DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
--- Create Student table
-
--- StudentID
-
--- StudentName
-
--- DOB
-
--- Gender
-
--- DepartmentID
-
--- Add constraints
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT NOT NULL,
+    UNIQUE (StudentName),
+    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+);
