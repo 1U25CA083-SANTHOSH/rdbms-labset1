6d3849663a62717828440088a181413d75f20c3a
1U25CA083-SANTHOSH <santhosh732008@gmail.com>
2026-08-17 12:41:30 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
--- Create Course Table
+
 CREATE TABLE Course (
     CourseID INT PRIMARY KEY,
     CourseName VARCHAR(100),
@@ -10,12 +10,12 @@ CREATE TABLE Course (
     DepartmentID INT
 );
 
--- Insert Records
+
 INSERT INTO Course (CourseID, CourseName, Credits, DepartmentID)
 VALUES
 (101, 'Database Management Systems', 4, 1),
 (102, 'Data Structures', 3, 2),
 (103, 'Computer Networks', 4, 3);
 
--- Display Table Structure
+
 DESCRIBE Course;
