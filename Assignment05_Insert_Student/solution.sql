991c20ece18074f9e2479ae303e6028801cfb281
1U25CA083-SANTHOSH <santhosh732008@gmail.com>
2026-08-17 12:41:59 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
-USE CollegeDB;
+
 
 CREATE TABLE Student(
     StudentID INT(5) PRIMARY KEY,
@@ -12,12 +12,12 @@ CREATE TABLE Student(
     PhoneNumber BIGINT
 );
 
--- Insert Records into Student Table
+
 INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
 VALUES
 (1001, 'Arun', 'Male', 101),
 (1002, 'Divya', 'Female', 102),
 (1003, 'Karthik', 'Male', 101);
 
--- Display All Student Records
+
 SELECT * FROM Student;
